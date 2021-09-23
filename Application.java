cc03f750decb4de1849a3dfbb06c49260d20a4f2
jcollinsstudent <joshuaolincollins@gmail.com>
2021-09-23 10:27:52 -0400

    Added documentation

--- Application.java
+++ Application.java
@@ -1,7 +1,20 @@
 import java.util.Stack;
 import java.util.Scanner;
+
+/**
+ * The application class contains the main method which accepts user input and executes the palindrome check logic.
+ *
+ * @author Joshua Collins
+ * @version 1.0
+ * @since 2021-9-16
+ */
 public class Application {
 	
+	/**
+	 * The main method contains the palindrome check logic and handles user input.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Stack<Character> charStack = new Stack<Character>();
 		
